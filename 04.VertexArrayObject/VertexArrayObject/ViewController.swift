36b0c46a8fe590705977caa9ad673edcbfbac94f
Burt.K <skyfe79@gmail.com>
2019-04-07 00:14:45 +0900

    Update 04.VertexArrayObject to Swfit5

--- 04.VertexArrayObject/VertexArrayObject/ViewController.swift
+++ 04.VertexArrayObject/VertexArrayObject/ViewController.swift
@@ -75,7 +75,7 @@ class ViewController: GLKViewController {
 extension ViewController {
     
     func setupGLcontext() {
-        glkView = self.view as! GLKView
+        glkView = (self.view as! GLKView)
         glkView.context = EAGLContext(api: .openGLES2)!
         EAGLContext.setCurrent(glkView.context)
     }
